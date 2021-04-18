b73f1f6bd75d6f6600688cb2b4225416fbf756b1
Gaurav Malhotra <gauravmalhotrain@gmail.com>
2021-04-18 14:08:13 +0530

    Site t1

---
+++ src/ContactUs/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Navbar from "../Homepage/Navbar";
+import Footer from "../Homepage/Footer";
+import "./styles.css";
+
+class ContactUs extends React.Component {
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className="contact-us">
+          <h1>CONTACT US</h1>
+          <h2>
+            Your health and satisfaction with our site are of equal importance
+            to us!
+          </h2>
+          <h2>Get in touch with us if you have any questions or feedback.</h2>
+          <div>
+            <h3>
+              Call Us: <a href="tel:8929987739">8929987739</a>
+            </h3>
+          </div>
+          <div>
+            <h3>(Available between 0900 hrs IST to 1700 hrs IST)</h3>
+          </div>
+          <div>
+            <h3>
+              Please allow upto 24 hours for a response and include your contact
+              information so we may respond back to you. Email Us:
+              srasneeru@gmail.com
+            </h3>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default ContactUs;
